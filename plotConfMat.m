cc64e863ab155ad754e64820af810475331d43c6
Vahe Tshitoyan <tsh.vahe@gmail.com>
2017-08-26 00:56:44 +0100

    added an accuracy measure in the title.

--- plotConfMat.m
+++ plotConfMat.m
@@ -34,7 +34,7 @@ confpercent = 100*confmat./repmat(sum(confmat, 1),numlabels,1);
 
 % plotting the colors
 imagesc(confpercent);
-title('Confusion Matrix');
+title(sprintf('Accuracy: %.2f%%', 100*trace(confmat)/sum(confmat(:))));
 ylabel('Output Class'); xlabel('Target Class');
 
 % set the colormap
